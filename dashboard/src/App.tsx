763b49c7711e6bbdcfc16f115a9e6676610b0a5c
Richard Drake <rdrake@rdrake.org>
2026-03-21 07:35:18 -0400

    feat: remove Setup page and AuthGuard, Cloudflare Access handles auth

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,8 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "next-themes";
-import AuthGuard from "@/components/AuthGuard";
 import Layout from "@/components/Layout";
-import Setup from "@/pages/Setup";
 import Dashboard from "@/pages/Dashboard";
 import BatchList from "@/pages/BatchList";
 import BatchDetail from "@/pages/BatchDetail";
@@ -18,19 +16,16 @@ export default function App() {
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       <BrowserRouter>
         <Routes>
-          <Route path="/setup" element={<Setup />} />
-          <Route element={<AuthGuard />}>
-            <Route element={<Layout />}>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/batches" element={<BatchList />} />
-              <Route path="/batches/new" element={<BatchNew />} />
-              <Route path="/batches/:id" element={<BatchDetail />} />
-              <Route path="/batches/:id/edit" element={<BatchEdit />} />
-              <Route path="/batches/:id/activities/new" element={<ActivityNew />} />
-              <Route path="/settings" element={<Settings />} />
-              <Route path="/tools" element={<Tools />} />
-              <Route path="/compare" element={<BatchComparison />} />
-            </Route>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/batches" element={<BatchList />} />
+            <Route path="/batches/new" element={<BatchNew />} />
+            <Route path="/batches/:id" element={<BatchDetail />} />
+            <Route path="/batches/:id/edit" element={<BatchEdit />} />
+            <Route path="/batches/:id/activities/new" element={<ActivityNew />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/tools" element={<Tools />} />
+            <Route path="/compare" element={<BatchComparison />} />
           </Route>
         </Routes>
       </BrowserRouter>
