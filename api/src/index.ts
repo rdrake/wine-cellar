e6cbe1d11e7c170b909f30a45c674dedbc818b85
Richard Drake <rdrake@rdrake.org>
2026-03-21 13:43:07 -0400

    feat: cron handler for 15-min alert sweep

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,3 +1,10 @@
 import app from "./app";
+import { evaluateAllBatches } from "./cron";
+import type { Bindings } from "./app";
 
-export default app;
+export default {
+  fetch: app.fetch,
+  async scheduled(event: ScheduledEvent, env: Bindings, ctx: ExecutionContext) {
+    ctx.waitUntil(evaluateAllBatches(env.DB));
+  },
+};
